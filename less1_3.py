2b75160f31bc67dc7d0b351b45a67b38d13edbe2
abyazov <abyazov2000@mail.ru>
2021-11-21 18:25:46 +0300

    second commit

--- less1_3.py
+++
@@ -1,7 +0,0 @@
-
-word1 = b"attribute"
-word2 = "класс".encode('utf-8')  # без encode кириллицу записать нельзя
-word3 = "функция".encode('utf-8')
-word4 = b"type"
-
-print(word1, word2, word3, word4)
